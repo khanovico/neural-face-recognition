580a9019483a5d51620ab6766fa7e0ebbb05f836
KhanovicDev <KhanovicDev@gmail.com>
2024-06-27 02:59:14 -0700

    fix var convention

--- face_Recognition.py
+++ face_Recognition.py
@@ -1,7 +1,7 @@
 
 # -*- coding: utf-8 -*-
 """
-@author: Krish.Naik
+@author: Khanovict
 """
 
 from keras.layers import Input, Lambda, Dense, Flatten
--- facefrontend.py
+++ facefrontend.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jan 15 14:49:19 2019
 
-@author: krish.naik
+@author: Khanovict
 """
 
 # Face Recognition
